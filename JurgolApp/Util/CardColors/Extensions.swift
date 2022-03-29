e3c55a823370516ec07eb46da77c8a35e3169fc3
Jlpezuc <jlpez@uc.cl>
2022-03-29 20:30:39 -0300

    Extension Int16

---
+++ JurgolApp/Util/CardColors/Extensions.swift
@@ -0,0 +1,26 @@
+//
+//  Extensions.swift
+//  JurgolApp
+//
+//  Created by Joaquín López Robertson on 29-03-22.
+//
+
+import Foundation
+import SwiftUI
+
+extension Int16 {
+    
+    var isEven: Bool {
+        if self % 2 == 0 {
+            return true
+        }
+        return false
+    }
+    
+    var isOdd: Bool {
+        if self % 2 != 0 {
+            return true
+        }
+        return false
+    }
+}
